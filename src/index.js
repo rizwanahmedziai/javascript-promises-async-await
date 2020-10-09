e89c7a5e70d43373ac8cf484c7ed8c6a6cfd244f
Rizwan Ahmed <rizwan@zekiexperts.com>
2020-10-09 15:20:01 +0500

    Finished Module 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export const fetchMovies = () => {};
+import { fetchWithTimeout } from './services';
+const movies = require('./data/movies.json');
+export function fetchMovies() {
+	const resolveFunction = () => movies;
+	return fetchWithTimeout(1000).then(resolveFunction);
+}
+
+const moviePromise = fetchMovies();
+
+moviePromise.then((results) => {
+	console.log(results);
+});
